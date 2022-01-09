f8e15d0b2f3466719f9bc9efcc31217fa6da9d99
wcx1998 <657370304@qq.com>
2022-01-09 13:42:38 -0800

    update

--- mileStone/src/main/java/com/chenxu/Test.java
+++ mileStone/src/main/java/com/chenxu/Test.java
@@ -1,8 +1,42 @@
 package com.chenxu;
 
-public class Test{
+import org.json.Cookie;
+import org.json.JSONObject;
 
-    static void main(String[] args) {
-        System.out.println("aaa");
+import java.io.*;
+
+import org.json.XML;
+public class Test{
+//XMLToJson
+
+
+    public static void main(String[] args) {
+        File file = new File(args[0]);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            StringBuilder file_string = new StringBuilder("");
+            while ((line = br.readLine())!= null){
+                file_string.append(line);
+                file_string.append(System.lineSeparator());
+            }
+            String xmlText = file_string.toString();
+            br.close();
+            // convert XML to JSON
+            JSONObject json = XML.toJSONObject(xmlText);
+            String jsonString = json.toString(4); //indent factor of 4 for pretty print
+            //write json object to file1.json
+            File fileFinal = new File("output1.json");
+            BufferedWriter bw = new BufferedWriter(new FileWriter(fileFinal));
+            bw.write(jsonString);
+            bw.flush();
+            bw.close();
+        }
+        catch(FileNotFoundException f) {
+            System.out.println("File not found");
+        }
+        catch(IOException ex) {
+            System.out.println("can not read file");
+        }
     }
 }
